6304d985e93a51d379dd24989a9e8ffd4e685c63
Ruskin Dantra <ruskin.dantra@gmail.com>
2018-10-19 11:17:42 +1300

    Initial commit

---
+++ ReactDemo/wwwroot/js/Tutorial.jsx
@@ -0,0 +1,49 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
+
+class CommentList extends React.Component {
+    render() {
+        return (
+            <div className="commentList">
+                <Comment author="Daniel Lo Nigro">
+                    Hello ReactJS.NET World!
+                </Comment>
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">
+                    This is *another* comment
+                </Comment>
+            </div>
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">Hello, world! I am a CommentForm.</div>
+        );
+    }
+}
+
+class Comment extends React.Component {
+    render() {
+        const md = new Remarkable();
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                {md.render(this.props.children.toString())}
+            </div>
+        );
+    }
+}

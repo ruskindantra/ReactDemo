ecf9bca93cba4acb3e12e4bebbfd1bf6a38b53d5
Ruskin Dantra <ruskin.dantra@gmail.com>
2018-10-23 17:02:43 +1300

    add event handlers

--- ReactDemo/wwwroot/js/Tutorial.jsx
+++ ReactDemo/wwwroot/js/Tutorial.jsx
@@ -27,9 +27,36 @@ class CommentList extends React.Component {
 }
 
 class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { author: '', text: '' };
+        this.handleAuthorChange = this.handleAuthorChange.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+    handleAuthorChange(e) {
+        this.setState({ author: e.target.value });
+    }
+    handleTextChange(e) {
+        this.setState({ text: e.target.value });
+    }
+    
     render() {
         return (
-            <div className="commentForm">Hello, world! I am a CommentForm.</div>
+            <form className="commentForm">
+                <input
+                    type="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 }
